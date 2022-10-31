34633f151e3dd662d65ae4d6b9f8a56b5e998840
GVindio <97354117+GVindio@users.noreply.github.com>
2022-10-30 22:27:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "AppDeployment" {
+resource "aws_instance" "jenkins_server" {
   ami             = "ami-09d3b3274b6c5d4aa"
   instance_type   = "t2.micro"
   key_name        = "testpem"
